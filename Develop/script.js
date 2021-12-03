b5b189112156b98f970474b9ee10629a3cc464c6
playground <connie.s.barrantes@gmail.com>
2021-12-03 13:41:53 -0500

    HTML time slot setup

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+// Declare variables
+
+
+// variable for current date
+
+
+//When I open the planner, the current day will display at the top of the calendar
+//When i scroll down, I am presented with time blocks for standard business hours
+//When I view the time blocks for that day, then each time block is colored-coded to indicate whether it is the past, present, or future
+// When I click into a time block, then i can enter an event
+//when i click the save button for that time block, then the text for that event is saved in local storage
+// when i refresh the page, then the saved events persist
+
