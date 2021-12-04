802f560c42abb316ba28c71f4639054917dd631e
playground <connie.s.barrantes@gmail.com>
2021-12-04 00:03:12 -0500

    Added color to time block depending on if event was in the past, present or future

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,26 @@
-let timeNow = moment().hours();
-
-
 $(document).ready(function () {
- 
- 
+    let timeNow = moment().hours();
 //When I open the planner, the current day will display at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
-})
 
-function hourTracker() {
-    //get current number of hours.
-    var currentHour = moment().hour(); // use of moment.js
+
+    function hourTracker() {
+      
 
     //When i scroll down, I am presented with time blocks for standard business hours
-
+    
     $(".time-block").each(function () {
-
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log( blockHour, currentHour)
-
+        
+        var hourSlot = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(hourSlot, currentHour)
+        
         //When I view the time blocks for that day, then each time block is colored-coded to indicate whether it is the past, present, or future
-        if (blockHour < currentHour) {
+        if (hourSlot < currentHour) {
             $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
         }
-        else if (blockHour === currentHour) {
+        else if (hourSlot === currentHour) {
             $(this).removeClass("past");
             $(this).addClass("present");
             $(this).removeClass("future");
@@ -40,8 +35,4 @@ function hourTracker() {
 hourTracker(); //re-run function
 
 
-
-// When I click into a time block, then i can enter an event
-//when i click the save button for that time block, then the text for that event is saved in local storage
-// $('.saveBtn').on('click', function() 
-// when i refresh the page, then the saved events persist
+})
