5b601a461193719c0839f8633b9e5c1d611ad679
playground <connie.s.barrantes@gmail.com>
2021-12-04 00:01:13 -0500

    current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,47 @@
-$document.ready(function () {
+let timeNow = moment().hours();
 
+
+$(document).ready(function () {
+ 
+ 
 //When I open the planner, the current day will display at the top of the calendar
-$("#currentDay").moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 })
 
+function hourTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); // use of moment.js
 
-//When i scroll down, I am presented with time blocks for standard business hours
-// $("#hour7 .description").val(localStorage.getItem("hour7"));
-// $("#hour8 .description").val(localStorage.getItem("hour8"));
-// $("#hour9 .description").val(localStorage.getItem("hour9"));
-// $("#hour10 .description").val(localStorage.getItem("hour10"));
-// $("#hour11 .description").val(localStorage.getItem("hour11"));
-// $("#hour12 .description").val(localStorage.getItem("hour12"));
-// $("#hour13 .description").val(localStorage.getItem("hour13"));
-// $("#hour14 .description").val(localStorage.getItem("hour14"));
-// $("#hour15 .description").val(localStorage.getItem("hour15"));
-// $("#hour16 .description").val(localStorage.getItem("hour16"));
-// $("#hour17 .description").val(localStorage.getItem("hour17"));
+    //When i scroll down, I am presented with time blocks for standard business hours
+
+    $(".time-block").each(function () {
+
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        //When I view the time blocks for that day, then each time block is colored-coded to indicate whether it is the past, present, or future
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); //re-run function
 
 
-//When I view the time blocks for that day, then each time block is colored-coded to indicate whether it is the past, present, or future
+
 // When I click into a time block, then i can enter an event
 //when i click the save button for that time block, then the text for that event is saved in local storage
+// $('.saveBtn').on('click', function() 
 // when i refresh the page, then the saved events persist
-
